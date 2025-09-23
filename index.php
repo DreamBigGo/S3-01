60ea213ab77cb3982ac30d594e4d969fcf9ed9fd
johann <johann.tixier91@gmail.com>
2025-09-24 00:33:27 +0200

    debut de la bar nac sauf que j'arrive pas a lancer le site car j'ai oublié comment on utilise uwamp

--- index.php
+++ index.php
@@ -5,7 +5,17 @@
     <title>Hello</title>
 </head>
 <body>
-<?php echo "Hello World!"; ?>
+<nav>
+    <img src="Asset/img/Logo_de_lorganisation_AGIRabcd.png" alt="Logo Agirabcd">
+    <a href="index.php">Accueil</a>
+    <a href="index.php">Qui somme nous</a>
+    <a href="index.php">Action france</a>
+    <a href="index.php">Action international</a>
+    <input class="styled" type="button" value="Soutenir AGIRabcd" />
+    <input class="styled" type="button" value="Solicité AGIRabcd"/>
+
+
+</nav>
 
 </body>
 </html>
