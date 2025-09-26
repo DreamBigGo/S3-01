23b773f30e43ea17403b3f03b3b03585743d610a
Satyam Peshwa <149512874+DreamBigGo@users.noreply.github.com>
2025-09-26 17:57:57 +0200

    Nav bar Johann TIXIER

--- index.php
+++ index.php
@@ -2,19 +2,18 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Hello</title>
+    <link rel="stylesheet" href="Asset/css/style.css">
+    <title> AgirAbcd </title>
 </head>
 <body>
 <nav>
-    <img src="Asset/img/Logo_de_lorganisation_AGIRabcd.png" alt="Logo Agirabcd">
-    <a href="index.php">Accueil</a>
-    <a href="index.php">Qui somme nous</a>
-    <a href="index.php">Action france</a>
-    <a href="index.php">Action international</a>
-    <input class="styled" type="button" value="Soutenir AGIRabcd" />
-    <input class="styled" type="button" value="Solicité AGIRabcd"/>
-
-
+        <img class="logo" src="Asset/img/Logo_de_lorganisation_AGIRabcd.png" alt="Logo Agirabcd">
+        <a href="index.php">Accueil</a>
+        <a href="index.php">Qui somme nous</a>
+        <a href="index.php">Action france</a>
+        <a href="index.php">Action international</a>
+        <input class="styled" type="button" value="Solicité AGIRabcd"/>
+        <input class="styled" type="button" value="Soutenir AGIRabcd" />
 </nav>
 
 </body>
