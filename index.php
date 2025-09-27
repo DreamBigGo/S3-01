bcc95d00e03ae0f8b904a9dc27fa9c23254e566b
Tiago-pereira-de-barros <tiago.debarros2006@gmail.com>
2025-09-27 12:53:57 +0200

    J'ai ajouté les image du carrousel dans le dossier image et mit la premieres image dans le site, je ferais la suite un peut plustars

--- index.php
+++ index.php
@@ -7,14 +7,17 @@
 </head>
 <body>
 <nav>
-        <img class="logo" src="Asset/img/Logo_de_lorganisation_AGIRabcd.png" alt="Logo Agirabcd">
-        <a href="index.php">Accueil</a>
-        <a href="index.php">Qui somme nous</a>
-        <a href="index.php">Action france</a>
-        <a href="index.php">Action international</a>
-        <input class="styled" type="button" value="Solicité AGIRabcd"/>
-        <input class="styled" type="button" value="Soutenir AGIRabcd" />
+    <img class="logo" src="Asset/img/Logo_de_lorganisation_AGIRabcd.png" alt="Logo Agirabcd">
+    <a href="index.php">Accueil</a>
+    <a href="index.php">Qui somme nous</a>
+    <a href="index.php">Action france</a>
+    <a href="index.php">Action international</a>
+    <input class="styled" type="button" value="Solicité AGIRabcd"/>
+    <input class="styled" type="button" value="Soutenir AGIRabcd" />
 </nav>
+    <div class="carrousel">
+        <img class="ImageCarrousel" src="Asset/img/img_Carrousel_1.png" alt="Image Carrousel">
+    </div>
 
 </body>
 </html>
