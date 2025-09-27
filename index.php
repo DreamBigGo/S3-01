914f2a99bb39e5370ccb29e81ecfbd8290b503dd
Tiago-pereira-de-barros <tiago.debarros2006@gmail.com>
2025-09-27 23:35:38 +0200

    j'ai commencer le footer, j'ai ajouté les réseau a gauche du footer, avec les lien qui permet en cliquant dans les réseau d'aller dans leurs page

--- index.php
+++ index.php
@@ -15,7 +15,43 @@
     <input class="styled" type="button" value="Solicité AGIRabcd"/>
     <input class="styled" type="button" value="Soutenir AGIRabcd" />
 </nav>
+<main>
 
+</main>
+<footer>
+    <div class="logoFooter">
+        <img src="Asset/img/Logo_AGIRabcd_carré.png" alt="Logo Agirabcd">
+        <p>AGIRabcd</p>
+    </div>
+    <div class="BarreLogo"></div>
+    <div class="Réseaux">
+        <a href="https://www.facebook.com/AGIRabcd/">
+            <img class="RéseauxImage" src="Asset/img/facebook.png" alt="Facebook">
+        </a>
+        <a href="https://www.youtube.com/channel/UCI3fcCXznu1c3IEKL4VuK4g">
+            <img class="RéseauxImage" src="Asset/img/youtube.png" alt="Youtube">
+        </a>
+        <a href="https://www.linkedin.com/company/agirabcd">
+            <img class="RéseauxImage" src="Asset/img/linkedle.png" alt="Linkedle">
+        </a>
+        <a href="index.php">
+            <img class="RéseauxImage" src="Asset/img/messagerie.png" alt="Messagerie">
+        </a>
+        <a href="https://www.google.com/maps/place/AGIRabcd/@48.8961438,2.3410647,17z/data=!3m1!4b1!4m6!3m5!1s0x47e66e15131b4f09:0x65a00bf02a9ab345!8m2!3d48.8961403!4d2.3436396!16s%2Fg%2F11c1v0csnn?authuser=0&entry=ttu&g_ep=EgoyMDI1MDkyNC4wIKXMDSoASAFQAw%3D%3D">
+            <img class="RéseauxImage" src="Asset/img/maps.png" alt="Google Maps">
+        </a>
+    </div>
+    <div>
+        <div class="TitreInfoFooter">
+            <p>Contact :</p>
+        </div>
+        <div class="BarreInfo"></div>
+        <div>
+            <p>8-26 rue Goubet 75019 PARIS</p>
+            <p>+33 1 47 70 18 90</p>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
 
