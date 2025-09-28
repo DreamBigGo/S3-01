1eb299d9eaedeb9a53dda4f51ef31072c20d778a
Tiago-pereira-de-barros <tiago.debarros2006@gmail.com>
2025-09-28 13:09:12 +0200

    j'ai ajouté une nouvelle partie au footer, c'est un peu plus lent que prevu

--- index.php
+++ index.php
@@ -19,37 +19,55 @@
 
 </main>
 <footer>
-    <div class="logoFooter">
-        <img src="Asset/img/Logo_AGIRabcd_carré.png" alt="Logo Agirabcd">
-        <p>AGIRabcd</p>
-    </div>
-    <div class="BarreLogo"></div>
-    <div class="Réseaux">
-        <a href="https://www.facebook.com/AGIRabcd/">
-            <img class="RéseauxImage" src="Asset/img/facebook.png" alt="Facebook">
-        </a>
-        <a href="https://www.youtube.com/channel/UCI3fcCXznu1c3IEKL4VuK4g">
-            <img class="RéseauxImage" src="Asset/img/youtube.png" alt="Youtube">
-        </a>
-        <a href="https://www.linkedin.com/company/agirabcd">
-            <img class="RéseauxImage" src="Asset/img/linkedle.png" alt="Linkedle">
-        </a>
-        <a href="index.php">
-            <img class="RéseauxImage" src="Asset/img/messagerie.png" alt="Messagerie">
-        </a>
-        <a href="https://www.google.com/maps/place/AGIRabcd/@48.8961438,2.3410647,17z/data=!3m1!4b1!4m6!3m5!1s0x47e66e15131b4f09:0x65a00bf02a9ab345!8m2!3d48.8961403!4d2.3436396!16s%2Fg%2F11c1v0csnn?authuser=0&entry=ttu&g_ep=EgoyMDI1MDkyNC4wIKXMDSoASAFQAw%3D%3D">
-            <img class="RéseauxImage" src="Asset/img/maps.png" alt="Google Maps">
-        </a>
-    </div>
-    <div>
-        <div class="TitreInfoFooter">
-            <p>Contact :</p>
+    <div class="Bloc1">
+        <div class="logoFooter">
+            <img src="Asset/img/Logo_AGIRabcd_carré.png" alt="Logo Agirabcd">
+            <p>AGIRabcd</p>
         </div>
-        <div class="BarreInfo"></div>
-        <div>
+        <div class="BarreLogo"></div>
+        <div class="Réseaux">
+            <a href="https://www.facebook.com/AGIRabcd/">
+                <img class="RéseauxImage" src="Asset/img/facebook.png" alt="Facebook">
+            </a>
+            <a href="https://www.youtube.com/channel/UCI3fcCXznu1c3IEKL4VuK4g">
+                <img class="RéseauxImage" src="Asset/img/youtube.png" alt="Youtube">
+            </a>
+            <a href="https://www.linkedin.com/company/agirabcd">
+                <img class="RéseauxImage" src="Asset/img/linkedle.png" alt="Linkedle">
+            </a>
+            <a href="index.php">
+                <img class="RéseauxImage" src="Asset/img/messagerie.png" alt="Messagerie">
+            </a>
+            <a href="https://www.google.com/maps/place/AGIRabcd/@48.8961438,2.3410647,17z/data=!3m1!4b1!4m6!3m5!1s0x47e66e15131b4f09:0x65a00bf02a9ab345!8m2!3d48.8961403!4d2.3436396!16s%2Fg%2F11c1v0csnn?authuser=0&entry=ttu&g_ep=EgoyMDI1MDkyNC4wIKXMDSoASAFQAw%3D%3D">
+                <img class="RéseauxImage" src="Asset/img/maps.png" alt="Google Maps">
+            </a>
+        </div>
+    </div>
+    <div class="Bloc2">
+        <div class="Contact">
+            <p>Contact :</p>
+            <div class="BarreInfo"></div>
             <p>8-26 rue Goubet 75019 PARIS</p>
             <p>+33 1 47 70 18 90</p>
         </div>
+        <div class="Autre">
+            <p>Autres :</p>
+            <div class="BarreInfo"></div>
+            <ul>
+                <li><a href="">Mentions légales</a></li>
+                <li><a href="">Plan du site</a></li>
+                <li><a href="">Administration</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="Bloc3">
+
+    </div>
+    <div class="Bloc4">
+
+    </div>
+    <div class="Bloc5">
+
     </div>
 </footer>
 </body>
