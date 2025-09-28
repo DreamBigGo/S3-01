e0d5d360a66da004e882629c89e46bc81495011c
Tiago-pereira-de-barros <tiago.debarros2006@gmail.com>
2025-09-28 17:31:33 +0200

    J'ai fini le footer

--- index.php
+++ index.php
@@ -6,70 +6,89 @@
     <title> AgirAbcd </title>
 </head>
 <body>
-<nav>
-    <img class="logo" src="Asset/img/Logo_de_lorganisation_AGIRabcd.png" alt="Logo Agirabcd">
-    <a href="index.php">Accueil</a>
-    <a href="index.php">Qui somme nous</a>
-    <a href="index.php">Action france</a>
-    <a href="index.php">Action international</a>
-    <input class="styled" type="button" value="Solicité AGIRabcd"/>
-    <input class="styled" type="button" value="Soutenir AGIRabcd" />
-</nav>
-<main>
+    <nav>
+        <img class="logo" src="Asset/img/Logo_de_lorganisation_AGIRabcd.png" alt="Logo Agirabcd">
+        <a href="index.php">Accueil</a>
+        <a href="index.php">Qui somme nous</a>
+        <a href="index.php">Action france</a>
+        <a href="index.php">Action international</a>
+        <input class="styled" type="button" value="Solicité AGIRabcd"/>
+        <input class="styled" type="button" value="Soutenir AGIRabcd" />
+    </nav>
+    <main>
 
-</main>
-<footer>
-    <div class="Bloc1">
-        <div class="logoFooter">
-            <img src="Asset/img/Logo_AGIRabcd_carré.png" alt="Logo Agirabcd">
-            <p>AGIRabcd</p>
+    </main>
+    <footer>
+        <div class="Bloc1">
+            <div class="logoFooter">
+                <img src="Asset/img/Logo_AGIRabcd_carré.png" alt="Logo Agirabcd">
+                <p>AGIRabcd</p>
+            </div>
+            <div class="BarreLogo"></div>
+            <div class="Réseaux">
+                <a href="https://www.facebook.com/AGIRabcd/">
+                    <img class="RéseauxImage" src="Asset/img/facebook.png" alt="Facebook">
+                </a>
+                <a href="https://www.youtube.com/channel/UCI3fcCXznu1c3IEKL4VuK4g">
+                    <img class="RéseauxImage" src="Asset/img/youtube.png" alt="Youtube">
+                </a>
+                <a href="https://www.linkedin.com/company/agirabcd">
+                    <img class="RéseauxImage" src="Asset/img/linkedle.png" alt="Linkedle">
+                </a>
+                <a href="index.php">
+                    <img class="RéseauxImage" src="Asset/img/messagerie.png" alt="Messagerie">
+                </a>
+                <a href="https://www.google.com/maps/place/AGIRabcd/@48.8961438,2.3410647,17z/data=!3m1!4b1!4m6!3m5!1s0x47e66e15131b4f09:0x65a00bf02a9ab345!8m2!3d48.8961403!4d2.3436396!16s%2Fg%2F11c1v0csnn?authuser=0&entry=ttu&g_ep=EgoyMDI1MDkyNC4wIKXMDSoASAFQAw%3D%3D">
+                    <img class="RéseauxImage" src="Asset/img/maps.png" alt="Google Maps">
+                </a>
+            </div>
         </div>
-        <div class="BarreLogo"></div>
-        <div class="Réseaux">
-            <a href="https://www.facebook.com/AGIRabcd/">
-                <img class="RéseauxImage" src="Asset/img/facebook.png" alt="Facebook">
-            </a>
-            <a href="https://www.youtube.com/channel/UCI3fcCXznu1c3IEKL4VuK4g">
-                <img class="RéseauxImage" src="Asset/img/youtube.png" alt="Youtube">
-            </a>
-            <a href="https://www.linkedin.com/company/agirabcd">
-                <img class="RéseauxImage" src="Asset/img/linkedle.png" alt="Linkedle">
-            </a>
-            <a href="index.php">
-                <img class="RéseauxImage" src="Asset/img/messagerie.png" alt="Messagerie">
-            </a>
-            <a href="https://www.google.com/maps/place/AGIRabcd/@48.8961438,2.3410647,17z/data=!3m1!4b1!4m6!3m5!1s0x47e66e15131b4f09:0x65a00bf02a9ab345!8m2!3d48.8961403!4d2.3436396!16s%2Fg%2F11c1v0csnn?authuser=0&entry=ttu&g_ep=EgoyMDI1MDkyNC4wIKXMDSoASAFQAw%3D%3D">
-                <img class="RéseauxImage" src="Asset/img/maps.png" alt="Google Maps">
-            </a>
+        <div class="Bloc2">
+            <div class="Contact">
+                <p>Contact :</p>
+                <div class="BarreInfo"></div>
+                <p>8-26 rue Goubet 75019 PARIS</p>
+                <p>+33 1 47 70 18 90</p>
+            </div>
+            <div class="Autre">
+                <p>Autres :</p>
+                <div class="BarreInfo"></div>
+                <ul>
+                    <li><a href="">Mentions légales</a></li>
+                    <li><a href="">Plan du site</a></li>
+                    <li><a href="">Administration</a></li>
+                </ul>
+            </div>
         </div>
-    </div>
-    <div class="Bloc2">
-        <div class="Contact">
-            <p>Contact :</p>
+        <div class="Bloc3">
+            <p>Délégation territoriales :</p>
             <div class="BarreInfo"></div>
-            <p>8-26 rue Goubet 75019 PARIS</p>
-            <p>+33 1 47 70 18 90</p>
+            <p><a href="">-ALLIER</a></p>
+            <p><a href="">-ALPES-MARITIMES</a></p>
         </div>
-        <div class="Autre">
-            <p>Autres :</p>
+        <div class="Bloc4">
+            <p>Informations utiles :</p>
             <div class="BarreInfo"></div>
             <ul>
-                <li><a href="">Mentions légales</a></li>
-                <li><a href="">Plan du site</a></li>
-                <li><a href="">Administration</a></li>
+                <li><a href="">Nos missions France</a></li>
+                <li><a href="">Nos mission à l’international</a></li>
+                <li><a href="">Nos relations européennes</a></li>
+                <li><a href="">Demande d’intervention</a></li>
+                <li><a href="">Abonnement à la lettre externe</a></li>
             </ul>
         </div>
-    </div>
-    <div class="Bloc3">
-
-    </div>
-    <div class="Bloc4">
-
-    </div>
-    <div class="Bloc5">
-
-    </div>
-</footer>
+        <div class="Bloc5">
+            <div class="Langue">
+                <a href="">
+                    <img src="Asset/img/Anglais.png" alt="Anglais">
+                </a>
+                <a href="">
+                    <img src="Asset/img/Espagnol.png" alt="Espagnol">
+                </a>
+            </div>
+            <p><a href="">Solicité AGIRabcd</a></p>
+        </div>
+    </footer>
 </body>
 </html>
 
