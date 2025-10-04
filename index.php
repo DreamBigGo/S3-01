c8d846a2909b206f098fb8fc5c72a198eef1a932
Satyam Peshwa <149512874+DreamBigGo@users.noreply.github.com>
2025-10-04 21:49:26 +0200

    Squashed commit of the following:
    commit 3740f54d132f09457a55095aaab683e005f41726
    Author: joh4hn <johann.tixier91@gmail.com>
    Date:   Fri Oct 3 12:27:45 2025 +0200
    
        Upgrade du footer plus fini les box france/inter
    
    commit 0596c851dbded27c782f47d7672a7c0be216d78f
    Author: joh4hn <johann.tixier91@gmail.com>
    Date:   Wed Oct 1 12:04:51 2025 +0200
    
        Upgrade des box france international
    
    commit 41a63d08433faca71447fdf6cd8d676371b628ec
    Author: joh4hn <johann.tixier91@gmail.com>
    Date:   Wed Oct 1 11:55:05 2025 +0200
    
        Upgrade du main
    
    commit fc24973efe3f322f44ae34c37350ff757b373e49
    Author: joh4hn <johann.tixier91@gmail.com>
    Date:   Wed Oct 1 11:31:29 2025 +0200
    
        J'ai upgrade la nav bar avec du php j'aidonc repasser le index en php et j'ai ajouter tout les autres page de la nav bar dans le site j'ai aussi crée des include pour la nav et le footer
    
    commit 5e3d69a68c64458c67bd94e63834fba73ba6e9d7
    Author: Satyam Peshwa <149512874+DreamBigGo@users.noreply.github.com>
    Date:   Tue Sep 30 15:02:59 2025 +0200
    
        Code du branch developpement
        (Footer accepter)
    
    # Conflicts:
    #	Asset/css/style.css
    #	index.html

---
+++ index.php
@@ -0,0 +1,45 @@
+
+<?php require_once 'Asset/page/header.php'; ?>
+
+
+<main>
+    <h1>AGIRabcd*, ONG reconnue d'utilité publique, mobilise les compétences de ses bénévoles au service des personnes fragilisées en France et des pays en développement dans le monde. </h1>
+    <p>* Association Générale des Intervenants Retraités pour les Actions de Bénévoles pour la Coopération et le Développement</p>
+    <a class="savoir" href="">En savoir Plus</a>
+
+    <div class="Presentation">
+        <ul class="france">
+            <li><h2>En France</h2></li>
+            <li><p>L’<a href="">autonomie</a> de chacun grâce à la maîtrise du français et du numérique </p></li>
+            <li><p>La <a href="">recherche d’emploi</a> ou l’aide à la création d’entreprise </p></li>
+            <li><p>La <a href="">mobilité territoriale</a> conduite d’un véhicule personnel ou usage des transports en commun </p></li>
+            <li><p>La <a href="">santé </a>et l'accès aux services de santé, la sortie de l'isolement social des personnes âgées. </p></li>
+            <div class="actions">
+            <a  href="index.html">
+                <input class="styled" type="button" value="Nos actions en france" />
+            </a>
+            </div>
+        </ul>
+        <ul class="international">
+
+                <li><h2>L'international</h2></li>
+                <li><p>Réalisation de <a href="">missions ponctuelles</a> ou des programmes spécifiques dans un domaine précis </p></li>
+                <li><p>Création de <a href="">projets de co-développement</a> impliquant de nombreux experts dans différents secteurs d’activités.
+                    </p>
+                </li>
+
+
+
+            <div class="actions">
+            <a  href="index.html">
+                <input class="styled" type="button" value="Nos actions à l'international" />
+            </a>
+            </div>
+        </ul>
+    </div>
+    <h1>Les actualités d'AGIRabLcd</h1>
+
+
+
+</main>
+<?php require_once 'Asset/page/footer.php' ?>
