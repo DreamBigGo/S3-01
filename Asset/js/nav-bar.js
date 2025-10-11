db9e7c4ec07f9c07b25fd0755da04dda22965fa0
Tiago-pereira-de-barros <tiago.debarros2006@gmail.com>
2025-10-11 18:18:10 +0200

    Ajout de la responsivité du main

--- Asset/js/nav-bar.js
+++ Asset/js/nav-bar.js
@@ -20,11 +20,13 @@ menuburger.addEventListener('change', () => {
     if (menuburger.checked) {
         navcache.style.display = 'flex';
         menuburgericon.src = '/S3-01/Asset/img/croix.png';
-        menuburgericon.style.height = '4em';
-        menuburgericon.style.width = '4em';
+        menuburgericon.style.marginTop = '0.9em';
+        menuburgericon.style.height = '50px';
+        menuburgericon.style.width = '50px';
     } else {
         navcache.style.display = 'none';
         menuburgericon.src = '/S3-01/Asset/img/MenuBurger.png';
+        menuburgericon.style.marginTop = '0';
         menuburgericon.style.height = '5em';
         menuburgericon.style.width = '5em';
     }
