91468e33a8194eea372d2de7bd45c699f3724b4f
DreamBigGo <satyampeshwa421@gmail.com>
2025-11-09 23:29:48 +0100

    Qui somme nous JOHANN

--- Asset/page/quisommenous.php
+++ Asset/page/quisommenous.php
@@ -1,7 +1,76 @@
 <?php require_once 'header.php'; ?>
 
     <main>
+        <h1>Qui somme nous</h1>
 
+        <p>
+            Une association de retraités, créée en 1983.
+            Des bénévoles de toutes catégories socio-professionnelles et de tous secteurs d’activité.
+        </p>
+
+        <div class="video">
+            <video controls width="250">
+                <source src=https://www.agirabcd.fr/COM-INTRAGIR/VIDEOS/AGIRabcd_en_2_mn.mp4" type="video/mp4" />
+        </div>
+
+        <h1>Ensemble, nous agissons</h1>
+
+        <p> Des actions en France favorisant l'insertion sociale et professionnelle de personnes de tout âge.
+            Des missions et des projets de co-développement dans les pays en développement, pour renforcer les capacités
+            locales en transmettant les compétences adaptées aux besoins des bénéficiaires.
+
+            Des actions en partenariat avec des acteurs institutionnels, des collectivités territoriales, des
+            entreprises et et des associations locales.
+        </p>
+
+        <img src="/Asset/img/qui%20somme%20nous%20.png" alt="information divers" class="imgethique">
+        <h1 class="hethique">Notre éthique</h1>
+
+        <div class="ethique">
+            <div class="blocethique">
+                <h2>La neutralité politique et confessionnelle et le respect de l'autre.</h2>
+                <p>AGIRabcd œuvre dans le respect de toutes les convictions et cultures.</p>
+            </div>
+
+            <div class="blocethique">
+                <h2>Pas de concurrence avec le secteur marchand</h2>
+                <p>AGIRabcd contribue à la création d'emplois sans se substituer à des emplois rémunérés.</p>
+            </div>
+
+            <div class="blocethique">
+                <h2>Aucune rémunération</h2>
+                <p>Les membres actifs sont des bénévoles. Les partenaires participent uniquement aux frais de structure.</p>
+            </div>
+
+            <div class="blocethique">
+                <h2>Rendre autonome</h2>
+                <p>Les interventions sur le terrain et à distance visent à rendre les populations autonomes et actrices de leur propre développement.</p>
+            </div>
+        </div>
+
+        <div class ="bene">
+            <h1>Le bénévolat sénior de compétences avec AGIRabcd</h1>
+            <p>
+                Le bénévolat sénior de compétences représente une ressource essentielle pour notre société, offrant un
+                potentiel significatif pour le développement économique, social et associatif.
+            </p>
+            <p>
+                <strong>Expertise et Contribution</strong>
+                Les seniors bénévoles apportent des compétences et une expérience riche qui peuvent dynamiser les
+                initiatives associatives, accompagner les jeunes, soutenir les entrepreneurs et contribuer à la cohésion
+                sociale.
+            </p>
+            <p>
+                <strong>Lien inter-générationnel</strong>
+                En restant actifs et en transmettant leur savoir, les seniors renforcent le lien intergénérationnel,
+                favorisant ainsi le maintien du lien social et la santé communautaire.
+            </p>
+            <p>
+                AGIRabcd fait partie du réseau <strong>Talents Seniors Bénévoles </strong>qui regroupe 7000 membres engagés à accompagner les
+                nouvelles générations, soutenir les entrepreneurs et les publics précaires à travers des actions porteuses
+                de savoirs et de solidarité à travers tout le territoire et à l'international..
+            </p>
+        </div>
     </main>
 
 <?php require_once 'footer.php' ?>
