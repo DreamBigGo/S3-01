bb41a0362a69637c45bbb15dd0828bcae4dd9b46
Satyam Peshwa <149512874+DreamBigGo@users.noreply.github.com>
2025-11-09 21:06:54 +0100

    Ajoute des pages personelle
    C'est mes pages personelle

---
+++ Asset/page/NousRejoindre.php
@@ -0,0 +1,51 @@
+<?php
+$titre="Nous rejoindre";
+require_once 'header.php'; ?>
+
+ <main>
+    <h1>Devenir Bénévole</h1>
+    <h3>Vous avez du temps libre et vous souhaitez mettre votre expérience au service d'une cause qui fait la différence ? <br>
+        Rejoignez notre association !</h3>
+    <video controls width="300">
+        <source src="https://www.agirabcd.fr/COM-INTRAGIR/VIDEOS/Devenir_Benevole_AGIRabcd.mp4" type="video/mp4" >
+    </video>
+    <h1>AVEC AGIRabcd VOUS POURREZ</h1>
+     <div class="Presentation">
+         <ul class="france">
+
+             <li> <h2>Aider concrètement des personnes qui en ont besoin </h2></li>
+             <li><h2>Nouer de liens de proximité  </h2></li>
+             <li><h2>Choisir votre engagement </h2></li>
+
+             <li><h4>Pour développer l'activité de votre choix, contactez votre délégation ou notre siège parisien</h4></li>
+             <div class="actions">
+                 <a href="Noustrouver.php" class="btn">
+                     <input class="styled" type="button" value="Nous rejoindre" >
+                 </a>
+             </div>
+         </ul>
+
+         <ul class="international">
+
+             <li><h2>Développer de nouvelles compétences</h2></li>
+             <li><h2>
+                     Découvrir de nouvelles cultures et/ou d'autres manières de travailler
+                 </h2>
+             </li>
+            <li><h2>Mettre votre expérience et vos competence au profit de la solidarité</h2></li>
+
+             <li><h4>Nous recherchons des bénévoles pour des activités spécifiques répertoriées sur la plateforme "Devenir bénévole"</h4></li>
+             <div class="actions">
+                 <a href="https://benevoles.agirabcd.eu/" class="btn">
+                     <input class="styled" type="button" value="Nos annonces de mission" >
+                 </a>
+                 </a>
+             </div>
+         </ul>
+     </div>
+
+
+
+</main>
+
+<?php require_once 'footer.php' ?>
