d3a87f833436956fa14959b2639a8162d0b21f69
johann <johann.tixier91@gmail.com>
2026-01-06 18:12:48 +0100

    Front : intégration vidéo et styles

--- public/Asset/page/NousRejoindre.php
+++ public/Asset/page/NousRejoindre.php
@@ -6,9 +6,11 @@ require_once 'header.php'; ?>
     <h1>Devenir Bénévole</h1>
     <h3>Vous avez du temps libre et vous souhaitez mettre votre expérience au service d'une cause qui fait la différence ? <br>
         Rejoignez notre association !</h3>
-    <video controls width="300">
+        <div class="video">
+    <video  controls width="300">
         <source src="https://www.agirabcd.fr/COM-INTRAGIR/VIDEOS/Devenir_Benevole_AGIRabcd.mp4" type="video/mp4" >
     </video>
+</div>
     <h1>AVEC AGIRabcd VOUS POURREZ</h1>
      <div class="Presentation">
          <ul class="france">
